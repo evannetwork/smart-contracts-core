511e68edda2485b02e1017626783733e2fdf8615
wulfraem <wulfraem@users.noreply.github.com>
2018-10-04 10:47:45 +0200

    Revert "Revert "add support for removing contract members the same way as inviting them""
    This reverts commit de7838d5bee0eebbeccd5e66e2025dd56af31cc7.

--- contracts/BaseContract.sol
+++ contracts/BaseContract.sol
@@ -110,7 +110,6 @@ contract BaseContract is BaseContractInterface, EnsReader {
         // if last bit is set, then invitee has set its known flag for msg.sender to true
         // assert((profile.getMappingValue(CONTACTS_LABEL, keccak256(msg.sender)) & 1) == 1);
 
-
         if (businessCenter != 0x0) {
             BusinessCenterInterface businessCenterInterface = BusinessCenterInterface(businessCenter);
             assert(businessCenterInterface.isMember(consumer));
@@ -133,7 +132,7 @@ contract BaseContract is BaseContractInterface, EnsReader {
         StateshiftEvent(uint(ConsumerState.Draft), consumer);
     }
 
-    function removeConsumer(address consumer) auth {
+    function removeConsumer(address consumer, address businessCenter) auth {
         assert(isConsumer(consumer));
 
         uint lastId = consumerCount--;
@@ -149,5 +148,17 @@ contract BaseContract is BaseContractInterface, EnsReader {
 
         DSRolesPerContract roles = DSRolesPerContract(authority);
         roles.setUserRole(consumer, MEMBER_ROLE, false);
+
+        if (businessCenter != 0x0) {
+            BusinessCenterInterface businessCenterInterface = BusinessCenterInterface(businessCenter);
+            assert(businessCenterInterface.isMember(consumer));
+            assert(!isConsumer(consumer));
+            businessCenterInterface.removeContractMember(this, consumer, contractType);
+        } else {
+            // trigger event from here if not attached to business businessCenter
+            EventHubBusinessCenter eventHub = EventHubBusinessCenter(getAddr(EVENTHUB_LABEL));
+            eventHub.sendContractEvent(
+                uint(EventHubBusinessCenter.BusinessCenterEventType.Cancel), contractType, this, consumer);
+        }
     }
 }

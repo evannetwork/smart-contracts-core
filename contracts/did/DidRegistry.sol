067d26f91f26819891b1799d4ec3289418817f38
Philip Kaiser <phiipkaiser30.05@gmail.com>
2020-02-07 09:13:54 +0100

    Added deactivateDid method
    [CORE-947]

--- contracts/did/DidRegistry.sol
+++ contracts/did/DidRegistry.sol
@@ -1,12 +1,12 @@
 /*
   Copyright (c) 2018-present evan GmbH.
- 
+
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
   You may obtain a copy of the License at
- 
+
       http://www.apache.org/licenses/LICENSE-2.0
- 
+
   Unless required by applicable law or agreed to in writing, software
   distributed under the License is distributed on an "AS IS" BASIS,
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,6 +27,7 @@ contract DidRegistry is Owned, EnsReader {
     // sha3('contractidentities')
     bytes32 public contractRegistryNode = 0xaca561d654b9355e105c347c1b404d12052bd568ed9c53ede94e3e2a3123cc3c;
     mapping(bytes32 => bytes32) public didDocuments;
+    mapping(bytes32 => bool) public deactivatedDids;
 
     /// @notice set DID document for a DID hash
     /// @param targetHash hash identity reference to set DID document hash for
@@ -40,7 +41,24 @@ contract DidRegistry is Owned, EnsReader {
             // allow if msg.sender is owner of a contract/alias identity
             IdentityHolder(getAddr(contractRegistryNode)).getOwner(targetHash) == msg.sender
         , 'lacking permissions to update DID document');
+        require(value != 0x0, 'Invalid value. For deactivating DIDs, please use the dedicated method.');
         didDocuments[targetHash] = value;
+    }
+
+    /// @notice deactivate DID document
+    /// @param targetHash hash identity reference to deactivate DID for
+    function deactivateDid(bytes32 targetHash) public {
+        require(
+            // allow if owner of registry, required for migration scenarios
+            msg.sender == owner ||
+            // allow if msg.sender === targetHash --> set own hash (called from identity)
+            bytes32(msg.sender) == targetHash ||
+            // allow if msg.sender is owner of a contract/alias identity
+            IdentityHolder(getAddr(contractRegistryNode)).getOwner(targetHash) == msg.sender
+        , 'lacking permissions to deactivate DID document');
+        require(didDocuments[targetHash] != 0x0, 'Did is not yet activated or has already been deactivated');
+        deactivatedDids[targetHash] = true;
+        didDocuments[targetHash] = 0x0;
     }
 
     /// @notice retrieve a single entry from a mapping
